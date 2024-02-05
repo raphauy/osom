210e450e826ab99ed2f61cf72efe1d2fb170cb58
raphauy <rapha.uy@rapha.uy>
2024-02-05 16:21:25 -0300

    small improve

--- src/app/admin/config/tokens-price.tsx
+++ src/app/admin/config/tokens-price.tsx
@@ -15,8 +15,8 @@ export default function TokensPrice({ clientId, promptTokensPrice, completionTok
         <div className="w-full p-4 border rounded-lg">
             <p className="text-2xl font-bold">Precio de tokens</p>
             <div className="flex items-end gap-4 pb-3 mb-3 border-b">
-                <p className="mt-5"><strong>Prompt Tokens Price:</strong> {promptTokensPrice ? Intl.NumberFormat("es-UY", { style: "currency", currency: "USD"}).format(promptTokensPrice) : "N/D"}</p>
-                <p className="mt-5"><strong>Completion Tokens Price:</strong> {completionTokensPrice ? Intl.NumberFormat("es-UY", { style: "currency", currency: "USD"}).format(completionTokensPrice) : "N/D"}</p>
+                <p className="mt-5"><strong>Prompt Tokens Price:</strong> {promptTokensPrice ? Intl.NumberFormat("es-UY", { style: "currency", currency: "USD", minimumFractionDigits: 3, maximumFractionDigits:3 }).format(promptTokensPrice) : "N/D"}</p>
+                <p className="mt-5"><strong>Completion Tokens Price:</strong> {completionTokensPrice ? Intl.NumberFormat("es-UY", { style: "currency", currency: "USD", minimumFractionDigits: 3, maximumFractionDigits:3}).format(completionTokensPrice) : "N/D"}</p>
                 <TokensPriceDialog title="Editar precios de tokens" trigger={editTrigger} id={clientId} />
             </div>
         </div>
